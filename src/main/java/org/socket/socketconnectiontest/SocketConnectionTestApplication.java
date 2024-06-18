58c8e7f00ddbda1b2a1867ad27223aa563b46e32
sharonita610 <sharonita610@gmail.com>
2024-06-18 11:43:41 +0900

    Initial commit

---
+++ src/main/java/org/socket/socketconnectiontest/SocketConnectionTestApplication.java
@@ -0,0 +1,13 @@
+package org.socket.socketconnectiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketConnectionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketConnectionTestApplication.class, args);
+	}
+
+}
